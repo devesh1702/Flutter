bf59f0cb926265072e426aa93e681e7739092d03
devesh1702 <deveshbhargava1702@gmail.com>
2023-05-18 21:52:13 +0530

    DAY 2 completed

---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Devesh Bhargava";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Center(
+        child: Text(
+          'Hello World there are $days! days in june and my name is $name',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
